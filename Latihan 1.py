b7b6a43942ee042633fd1f4371b8b8dfdf77f988
SyahrulMahfudy <136712483+SyahrulMahfudy@users.noreply.github.com>
2023-06-16 20:04:44 +0700

    Latihan 1.py
    Latihan 1 Algotimapemrograman

---
+++ Latihan 1.py
@@ -0,0 +1,14 @@
+
+nim = input("Masukan Nim Anda: ")
+nama = input("Masukkan Nama Lengkap: ")
+kelas = input("Masukkan Kelas Anda: ")
+usia = input(" Masukkan usia: ")
+print ("=====================================")
+print (" IDENTITAS MAHASISWA")
+print ("=====================================")
+print ("Nim Mahasiswa :",nim)
+print ("Nama Lengkap  :",nama)
+print ("Kelas Saat Ini:",kelas)
+print ("Usia Anda     :", usia)
+print ("=====================================")
+
